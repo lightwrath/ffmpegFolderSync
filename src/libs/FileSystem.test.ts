245a9b5267d5ac4ec917f68d9c63941dcde37982
Kyle Biggins <lightwrath@Kyles-MacBook-Pro.local>
2025-05-01 01:23:21 +0300

    add tests for file type checking

--- src/libs/FileSystem.test.ts
+++ src/libs/FileSystem.test.ts
@@ -2,6 +2,10 @@ import { describe, it, expect } from "bun:test";
 import FileSystem from "./FileSystem.ts";
 
 describe("FileSystem", () => {
+    it('should return the folder type', async () => {
+        expect(await FileSystem.type(import.meta.dir)).toBe("directory");
+        expect(await FileSystem.type(`${import.meta.dir}/${import.meta.file}`)).toBe("file");
+    })
     it('should create a target path based on source path and provided target', () => {
         const filePath = "/Vault/Media/legacy/tv/Curb Your Enthusiasm/Season 01/Episode Name S01E01.mkv"
         const source = "/Vault/Media/legacy/tv"
--- src/libs/FileSystem.ts
+++ src/libs/FileSystem.ts
@@ -2,22 +2,25 @@ import { readdir } from "node:fs/promises"
 
 export default class FileSystem {
     public static async type(path: string) {
+        console.log(path);
         try {
             await readdir(path)
             return "directory"
-        } catch (e) {}
+        } catch (e) {
+            console.error(e)
+        }
         const file = Bun.file(path)
         if (await file.exists()) {
             return "file"
         }
         return "unknown"
     }
-    
+
     public static async ls(path: string) {
         if (await this.type(path) !== "directory") throw new Error("path must be a directory")
         return readdir(path)
     }
-    
+
     public static async lsFilesRecursive(path: string) {
         let files: Array<string> = []
         if (await this.type(path) === "directory") {
@@ -30,7 +33,7 @@ export default class FileSystem {
         }
         return files
     }
-    
+
     public static pathSourceToTarget(source: string, target: string, filePath: string) {
         return filePath.replace(source, target);
     }
