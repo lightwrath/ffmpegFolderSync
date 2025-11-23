a1bb5bf6236b98aa14272fa324d9ec6077102ab0
Kyle Biggins <lightwrath@Kyles-MacBook-Pro.local>
2025-11-23 21:35:08 +0200

    pipe stdout and err to the log file

--- src/libs/Process.ts
+++ src/libs/Process.ts
@@ -6,6 +6,22 @@ export default class Process {
     constructor(cmd: Array<string>) {
         Log.info("Process started with: " + cmd.join(" "))
         this.subprocess = Bun.spawn({ cmd, stderr: "pipe" })
+        this.logStdout().catch((err) => Log.error(err)).finally(() => Log.info("Process stdout closed"))
+        this.logStderr().catch((err) => Log.error(err)).finally(() => Log.info("Process stderr closed"))
+    }
+
+    private async logStdout() {
+        if (!this.subprocess.stdout || typeof this.subprocess.stdout == "number" ) return Log.error("No stdout stream for process")
+        for await (const stdout of this.subprocess.stdout) {
+            Log.info(new TextDecoder().decode(stdout));
+        }
+    }
+
+    private async logStderr() {
+        if (!this.subprocess.stderr || typeof this.subprocess.stderr == "number" ) return Log.error("No stderr stream for process")
+        for await (const stderr of this.subprocess.stderr) {
+            Log.info(new TextDecoder().decode(stderr));
+        }
     }
 
     public awaitExit() {
